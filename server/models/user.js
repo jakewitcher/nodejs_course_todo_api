40ccd9054e5061266d9534c08f1972c081b6947d
Jake Witcher <jjwitcher09@gmail.com>
2018-09-07 23:25:38 -0400

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -62,11 +62,29 @@ UserSchema.statics.findByToken = function (token) {
     return Promise.reject();
   }
 
-  //return User.findById(decoded._id);
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
